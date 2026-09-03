d8ce25e82050f3229f84f463dcc8c2cfdb585e7c
nabilfkrr <fikrin868@gmail.com>
2026-09-03 10:22:04 +0700

    praktikum pekan1

---
+++ Pekan1/Program1_2511533011.py
@@ -0,0 +1,13 @@
+#Program pertama
+"""
+Komentar
+dua baris atau lebih 
+pertemuan 1 praktikum alpro
+"""
+print("Hello World")
+print()
+print("Program ini menghasilkan")
+print("empat baris output")
+print("/ \\// \\\\ /// \\\\\\")
+print("program ini " + " dibuat oleh a/n"
+       + " Nabil Fikri 2511533011")
